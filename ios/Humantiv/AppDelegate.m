2b2b1ad987d8ac6d281b6cc3d84a78bc15cc6e44
Nabyl Bennouri <nbennouri@car.fit>
2018-02-10 18:24:06 -0800

    iOS build

--- ios/Humantiv/AppDelegate.m
+++ ios/Humantiv/AppDelegate.m
@@ -37,7 +37,8 @@
   [[FBSDKApplicationDelegate sharedInstance] application:application
                            didFinishLaunchingWithOptions:launchOptions];
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+ // jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"Humantiv"
                                                initialProperties:nil
