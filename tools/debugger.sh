bd7e565468781f7b657c22e69da16087c912541c
Nabyl Bennouri <nabyl@liveutrahealthy.com>
2018-09-20 13:55:52 -0700

    Updated babel-core + tools

---
+++ tools/debugger.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Nabyl Bennouri - 09/17/2018
+
+react-native-debugger-open
---
+++ tools/visualizer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Nabyl Bennouri - 09/17/2018
+
+./node_modules/.bin/react-native-bundle-visualizer
