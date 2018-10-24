1fb7b499f93790fe9120a9d0780456428ddad78b
Nabyl Bennouri <nabyl@liveutrahealthy.com>
2018-10-24 00:06:55 -0700

    Fix pre install script

--- npmpreinstall.sh
+++ npmpreinstall.sh
@@ -3,9 +3,34 @@ if [ ! -L ~/Documents/FacebookSDK ] && [ ! -d ~/Documents/FacebookSDK ]; then
     echo 'Symlinking ~/Documents/FacebookSDK to Facebook SDK in repo'
     ln -s $(cd ios/Facebook; pwd) ~/Documents/FacebookSDK
 fi
+
 # Fix for code push with React Native 0.56
-mkdir android/app
-mkdir android/app/build
-mkdir android/app/build/intermediates
-mkdir android/app/build/intermediates/assets
-mkdir android/app/build/intermediates/assets/debug
+APPDIR="android/app"
+if [ ! -d "$APPDIR" ]; then
+  echo 'Creating' $APPDIR
+  mkdir $APPDIR
+fi
+
+BUILDDIR="android/app/build"
+if [ ! -d "$BUILDDIR" ]; then
+  echo 'Creating' $BUILDDIR
+  mkdir $BUILDDIR
+fi
+
+INTERMEDIATESDIR="android/app/build/intermediates"
+if [ ! -d "$INTERMEDIATESDIR" ]; then
+  echo 'Creating' $INTERMEDIATESDIR
+  mkdir $INTERMEDIATESDIR
+fi
+
+ASSETSDIR="android/app/build/intermediates/assets"
+if [ ! -d "$ASSETSDIR" ]; then
+  echo 'Creating' $ASSETSDIR
+  mkdir $ASSETSDIR
+fi
+
+DEBUGDIR="android/app/build/intermediates/assets/debug"
+if [ ! -d "$DEBUGDIR" ]; then
+  echo 'Creating' $DEBUGDIR
+  mkdir $DEBUGDIR
+fi
