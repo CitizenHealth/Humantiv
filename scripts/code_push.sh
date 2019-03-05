5475e800ca8af2f419a6197aeacc7114975ea2fa
Nabyl Bennouri <nabyl@liveutrahealthy.com>
2019-03-05 12:16:39 -0800

    Add code push script

--- scripts/code_push.sh
+++ scripts/code_push.sh
@@ -1,4 +1,37 @@
 #!/usr/bin/env bash
 
-# Display app Keys
-appcenter codepush deployment list -a Citizen-Health/Humantiv --displayKeys
+echo Do you want to display the keys or deploy? [display/deploy]
+read option
+echo What platform? [ios/android]
+read os
+while [ $os -ne 'ios' ] && [$os -ne 'android']
+do
+	read os
+done
+
+if [ $option == 'display' ]
+then
+	if [ $os == 'ios' ]
+	then
+		# Display app Keys
+		appcenter codepush deployment list -a Citizen-Health/Humantiv --displayKeys
+	elif [ $os == 'android' ]
+	then
+		# Display app Keys
+		appcenter codepush deployment list -a Citizen-Health/Humantiv-1 --displayKeys
+	fi	
+elif [ $option == 'deploy' ]
+then
+	if [ $os == 'ios' ]
+	then
+		# Deploy iOS app
+		appcenter codepush release-react -a Citizen-Health/Humantiv -d Production
+	elif [ $os == 'android' ]
+	then
+		# Deploy Android app
+		appcenter codepush release-react -a Citizen-Health/Humantiv-1 -d Production
+	fi
+else
+	# Display app Keys
+	echo Bad argument. Only 'display' and 'deploy' are accepted.
+fi
