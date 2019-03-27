066fc34ec24aefd922a11426791028d349d31dc5
Nabyl Bennouri <nabyl@liveutrahealthy.com>
2019-03-26 18:47:07 -0700

    Fix Beta Wallet help button

--- src/components/WalletView.js
+++ src/components/WalletView.js
@@ -504,7 +504,8 @@ const styles = StyleSheet.create({
       marginTop: 10,
       marginLeft: 10,
       marginRight: 10,
-      borderRadius: 3
+      borderRadius: 3,
+      flex: 1
     }, 
     textStyle: {
         flex: 4,
