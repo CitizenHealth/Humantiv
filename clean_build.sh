2ba5e5c6e22acd9798f84076aa2c18a033f58bbd
Nabyl Bennouri <nabyl@liveutrahealthy.com>
2019-04-02 16:17:04 -0700

    Update clean script

--- clean_build.sh
+++ clean_build.sh
@@ -5,6 +5,6 @@ rm -rf ./node_modules
 rm package-lock.json
 npm install
 rm -rf $TMPDIR/react-* $TMPDIR/metro-* $TMPDIR/haste-*
-react-native link
-#rm -rf $TMPDIR/haste-map-react-native-packager-*
+#react-native link
+rm -rf $TMPDIR/haste-map-react-native-packager-*
 npm start -- --reset-cache
