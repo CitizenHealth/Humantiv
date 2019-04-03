d377c7efc738848c3212364aa6ec48908e5b4425
Nabyl Bennouri <nabyl@liveutrahealthy.com>
2019-04-02 23:41:52 -0700

    Added swipe support for tab view on Android

--- android/app/src/main/java/io/citizenhealth/humantiv/MainActivity.java
+++ android/app/src/main/java/io/citizenhealth/humantiv/MainActivity.java
@@ -1,6 +1,9 @@
 package io.citizenhealth.humantiv;
 import android.content.Intent;
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -17,5 +20,15 @@ public class MainActivity extends ReactActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+          @Override
+          protected ReactRootView createRootView() {
+           return new RNGestureHandlerEnabledRootView(MainActivity.this);
+          }
+        };
     }
 }
